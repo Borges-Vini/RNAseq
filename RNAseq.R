2ca96dc5ead8e3c0e0ff359ca1ad543344deb4b3
Borges-Vini <132589662+Borges-Vini@users.noreply.github.com>
2025-03-19 13:55:19 -0400

    Adding MultiQC step

--- RNAseq.R
+++ RNAseq.R
@@ -30,22 +30,30 @@ invisible(lapply(packages, function(pkg) {
 args <- commandArgs(trailingOnly=TRUE)
 parfilepath <- args[1]
 parfilepath <- "/Users/vborges/RNAseq/RNAseq.par"
-if (!file.exists(parfilepath)) {stop("Error: The file does not exist at the specified path: ", parfilepath)} else {print("Reading parameter file")}
+if (!file.exists(parfilepath)) {stop("Error: The file does not exist at the specified path: ", parfilepath)
+  } else {print("Reading parameter file")}
 parfile <- Filter(function(x) !grepl("^#", x) && nzchar(x), trimws(readLines(parfilepath, warn = FALSE)))
 
+##### FastQC #####
 
+##### Summarizing QC Results #####
 
+if (parfile[2] %in% c("YES", "yes", "Yes")) { 
+  
+#multiqc - Checking and installing
+  venv_path <- "multiqc_env"
+  if (!file.exists(venv_path)) {
+    virtualenv_create(venv_path)}
+  virtualenv_install(venv_path, "multiqc")
+  use_virtualenv(venv_path, required = TRUE)
+  multiqc_path <- file.path(path.expand("~"), ".virtualenvs", venv_path, "bin", "multiqc")
+  system(paste(multiqc_path, "--version"))
 
-##### Checking installation #####
-
-#Multiqc
-venv_path <- "multiqc_env"
-if (!file.exists(venv_path)) {
-  virtualenv_create(venv_path)}
-virtualenv_install(venv_path, "multiqc")
-use_virtualenv(venv_path, required = TRUE)
-multiqc_path <- file.path(path.expand("~"), ".virtualenvs", venv_path, "bin", "multiqc")
-system(paste(multiqc_path, "--version"))
-
-
+#fastq - Checking and Running
+  if (!file.exists(parfile[1])) { stop("Error: The folder does not exist at the specified path: ", parfile[1]) }
+  fastq_files <- list.files(parfile[1], pattern = "\\.fastq(\\.gz)?$", full.names = TRUE, recursive = TRUE)
+  if (length(fastq_files) == 0) {stop("Error: No .fastq files found in the folder: ", parfile[1])
+  } else { print("Valid folder with .fastq files found. Proceeding with analysis.")
+  system(paste(multiqc_path, parfile[1],"-o", parfile[1]))}
+}
 
